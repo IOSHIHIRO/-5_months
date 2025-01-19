49a49cf6e1b01fe485001fd1f7f0bbb784268b74
IOSHIHIRO <emirans23@gmail.com>
2025-01-19 20:02:21 +0600

    dz

--- Afisha/urls.py
+++ Afisha/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from movie_app import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/directors/', views.director_list_api),
-    path('api/v1/directors/<int:id>/', views.director_detail_view),
-    path('api/v1/movies/', views.movie_list_view),
-    path('api/v1/movies/<int:id>/', views.movies_detail_view),
-    path('api/v1/reviews/', views.review_list_view),
-    path('api/v1/reviews/<int:id>/', views.reviews_detail_view),
+    path('', include('movie_app.urls')),
+    path('', include('users.urls')),
 ]
---
+++ movie_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from movie_app import views
+
+urlpatterns = [
+    path('api/v1/directors/', views.director_list_api),
+    path('api/v1/directors/<int:id>/', views.director_detail_view),
+    path('api/v1/movies/', views.movie_list_view),
+    path('api/v1/movies/<int:id>/', views.movies_detail_view),
+    path('api/v1/reviews/', views.review_list_view),
+    path('api/v1/reviews/<int:id>/', views.reviews_detail_view)
+
+]
